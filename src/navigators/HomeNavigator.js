7beb3d3cc2f1c7f33018399467d50a07c6b86729
Abhishek0706 <patil.abhishekpatil108@gmail.com>
2022-02-07 16:35:11 +0530

    added logout

--- src/navigators/HomeNavigator.js
+++ src/navigators/HomeNavigator.js
@@ -1,39 +1,57 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useDispatch } from 'react-redux';
+import { Platform, StyleSheet } from 'react-native';
 
 import DoorScreen from '../screens/Home/DoorScreen';
 import ChatScreen from '../screens/Home/ChatScreen';
 import Colors from '../constants/Colors';
-import { Platform } from 'react-native';
+import Button from '../components/UI/Button';
+import { logout } from '../redux/actions/auth';
 
 const Stack = createNativeStackNavigator();
 
 const HomeNavigator = () => {
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
-    <Stack.Navigator initialRouteName="Door">
+    <Stack.Navigator
+      initialRouteName="Door"
+      screenOptions={{
+        headerRight: () => (
+          <Button
+            title="Logout"
+            onPress={logoutHandler}
+            style={styles.logoutButtonContainer}
+          />
+        ),
+        headerTitleAlign: 'left',
+        headerStyle: {
+          backgroundColor:
+            Platform.OS === 'android'
+              ? Colors.primaryColor
+              : Colors.secondaryColor,
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : 'black',
+      }}>
       <Stack.Screen
         name="Door"
         component={DoorScreen}
-        options={{
-          headerShown: false,
-        }}
+        options={{ title: 'Awesome Chat App' }}
       />
-      <Stack.Screen
-        name="Chat"
-        component={ChatScreen}
-        options={{
-          headerTitleAlign: 'center',
-          headerStyle: {
-            backgroundColor:
-              Platform.OS === 'android'
-                ? Colors.primaryColor
-                : Colors.secondaryColor,
-          },
-          headerTintColor: Platform.OS === 'android' ? 'white' : 'black',
-        }}
-      />
+      <Stack.Screen name="Chat" component={ChatScreen} />
     </Stack.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  logoutButtonContainer: {
+    backgroundColor: Colors.logoutButton,
+  },
+});
 
 export default HomeNavigator;
